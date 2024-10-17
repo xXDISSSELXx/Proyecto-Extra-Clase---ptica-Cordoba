197d1f0cc442ae27e6fb4c035613c06f576e9523
davidlop <davidloperahenao@gmail.com>
2024-10-17 09:29:51 -0500

    IdHelper added.

--- src/co/edu/opticacordoba/helpers/IdHelper.java
+++ src/co/edu/opticacordoba/helpers/IdHelper.java
@@ -1,5 +1,30 @@
 package co.edu.opticacordoba.helpers;
 
 public class IdHelper {
+	
+	private static final String DEFAULT_ID = "1";
+	
+	public static final Integer convertToNumber(final String idAsString) {
+		return TextHelper.applyTrim(idAsString).matches("\\d+") ?  Integer.parseInt(idAsString) : getDefault();
+	}
 
+	public static final Integer getDefault(final Integer value, final Integer defaultValue) {
+		return ObjectHelper.getDefault(value, defaultValue);
+	}
+	
+	public static final Integer getDefault() {
+		return Integer.parseInt(DEFAULT_ID);
+	}
+	
+	public static final String getDefaultAsSring() {
+		return DEFAULT_ID;
+	}
+	
+	public static final boolean isDefault(final Integer value) {
+		return getDefault(value, getDefault()).equals(getDefault());
+	}
+	
+	public static final boolean isDefault(final String idAsString) {
+		return getDefault(convertToNumber(idAsString), getDefault()).equals(getDefault());
+	}
 }

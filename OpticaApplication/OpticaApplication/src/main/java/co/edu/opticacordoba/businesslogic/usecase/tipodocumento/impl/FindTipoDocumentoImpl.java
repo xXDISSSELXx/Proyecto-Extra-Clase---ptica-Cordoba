c966cc29ed113b31643f7c210b2fd9ea8f5cae03
davidlop <davidloperahenao@gmail.com>
2024-10-24 16:05:07 -0500

    TipoDocumento Userecases Implementationsadded.

---
+++ OpticaApplication/OpticaApplication/src/main/java/co/edu/opticacordoba/businesslogic/usecase/tipodocumento/impl/FindTipoDocumentoImpl.java
@@ -0,0 +1,16 @@
+package co.edu.opticacordoba.businesslogic.usecase.tipodocumento.impl;
+
+import java.util.List;
+
+import co.edu.opticacordoba.businesslogic.usecase.tipodocumento.FindTipoDocumento;
+import co.edu.opticacordoba.domain.TipoDocumentoDomain;
+
+public class FindTipoDocumentoImpl implements FindTipoDocumento{
+
+	@Override
+	public List<TipoDocumentoDomain> execute(TipoDocumentoDomain data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ OpticaApplication/OpticaApplication/src/main/java/co/edu/opticacordoba/businesslogic/usecase/tipodocumento/impl/RegisterNewTipoDocumentoImpl.java
@@ -0,0 +1,14 @@
+package co.edu.opticacordoba.businesslogic.usecase.tipodocumento.impl;
+
+import co.edu.opticacordoba.businesslogic.usecase.tipodocumento.RegisterNewTipoDocumento;
+import co.edu.opticacordoba.domain.TipoDocumentoDomain;
+
+public class RegisterNewTipoDocumentoImpl implements RegisterNewTipoDocumento{
+
+	@Override
+	public void execute(TipoDocumentoDomain data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

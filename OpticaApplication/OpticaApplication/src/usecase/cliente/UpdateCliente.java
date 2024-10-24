9922cbe5c5dd1c5938f57743f3f989bbdfc345fd
davidlop <davidloperahenao@gmail.com>
2024-10-24 15:59:01 -0500

    Cliente Usercases added.

---
+++ OpticaApplication/OpticaApplication/src/usecase/cliente/UpdateCliente.java
@@ -0,0 +1,8 @@
+package co.edu.opticacordoba.businesslogic.usecase.cliente;
+
+import co.edu.opticacordoba.businesslogic.usecase.UseWithoutReturn;
+import co.edu.opticacordoba.domain.TipoDocumentoDomain;
+
+public interface UpdateCliente extends UseWithoutReturn<TipoDocumentoDomain>{
+
+}
